211706db1023f971f357372e19f9780287eb7597
kyte-voileers <57146420+kyte-voileers@users.noreply.github.com>
2023-02-23 12:34:36 +0100

    1 binaries uploaded

---
+++ src/data/index.js
@@ -0,0 +1,45 @@
+const data = [
+    {      
+        repoName: 'Space Shooter',
+        repoNameURL: 'space-shooter-game',
+        breakPointPx: 600,
+        imageWidthMobile: '90vw',
+        imageWidthDesktop: '50vw',
+        deployURL: "https://ivanderlich-space-shooter.netlify.app/",
+        textLeft: false,
+    },{      
+        repoName: 'Fakebook',
+        repoNameURL: 'fakebook',
+        breakPointPx: 740,
+        imageWidthMobile: '90vw',
+        imageWidthDesktop: '50vw',
+        deployURL: "https://afternoon-wave-75260.herokuapp.com/",
+        textLeft: true,
+    }, {      
+        repoName: 'Weather App',
+        repoNameURL: 'weather-app',
+        breakPointPx: 740,
+        imageWidthMobile: '90vw',
+        imageWidthDesktop: '50vw',
+        deployURL: "http://ivanderlich-weather-app.surge.sh/",
+        textLeft: false,
+    },{      
+        repoName: 'Todo App',
+        repoNameURL: 'to-do',
+        breakPointPx: 740,
+        imageWidthMobile: '90vw',
+        imageWidthDesktop: '50vw',
+        deployURL: "https://ivanderlich.github.io/to-do/",
+        textLeft: true,
+    }, {      
+        repoName: 'Calculator App',
+        repoNameURL: 'calculator',
+        breakPointPx: 740,
+        imageWidthMobile: '90vw',
+        imageWidthDesktop: '50vw',
+        deployURL: "https://ivanderlich-calculator.herokuapp.com/",
+        textLeft: false,
+    }
+]
+
+export default data;
